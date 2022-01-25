418c4d20573a62a8edf019fc83b1f0b53599eb13
jhigatzberger <91291245+jhigatzberger@users.noreply.github.com>
2022-01-25 11:52:14 +0100

    StartEntityMarker reduced

--- Assets/Behaviour/Spawning/StartEntityMarker.cs
+++ Assets/Behaviour/Spawning/StartEntityMarker.cs
@@ -2,50 +2,20 @@ using UnityEngine;
 
 namespace JHiga.RTSEngine.Spawning
 {
-    [ExecuteAlways]
     public class StartEntityMarker : MonoBehaviour
-    {
-        public StartEntityData data;
-        public FactionProperties faction;
-        private FactionProperties parent;
-        public GameEntityPool toSpawn;
+    { 
+        [SerializeField] private FactionProperties faction;
+        [SerializeField] private EntityPool toSpawn;        
         private void Awake()
-        {
-            if (Application.isPlaying)
-                Destroy(gameObject);
-
-            if (data == null)
+        {         
+            StartEntityData data = new StartEntityData
             {
-                data = new StartEntityData
-                {
-                    entity = toSpawn,
-                    offsetPosition = transform.position
-                };
-            }
+                entity = toSpawn,
+                offsetPosition = transform.position
+            };
+            faction.startEntities.Add(data); 
+            Destroy(gameObject);
         }
-
-        private void OnDestroy()
-        {
-            if (!Application.isPlaying)
-                parent.startEntities.Remove(data);
-        }
-
-        private void OnDrawGizmosSelected()
-        {
-            if(parent != faction)
-            {
-                if (parent != null && parent.startEntities.Contains(data))
-                    parent.startEntities.Remove(data);
-                parent = faction;
-                if (parent != null)
-                    parent.startEntities.Add(data);
-            }
-            if (data.offsetPosition != transform.position)
-                data.offsetPosition = transform.position;
-            if (data.entity != toSpawn)
-                data.entity = toSpawn;
-        }
-
         private void OnDrawGizmos()
         {
             if(toSpawn != null && toSpawn.prefab != null)
